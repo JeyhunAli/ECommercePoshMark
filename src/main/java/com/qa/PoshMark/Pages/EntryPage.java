0c3b1d4fb5e9a473bc9d121218165f15c85a35f3
jey <jey@jeys-mbp.home>
2019-11-05 18:58:11 -0500

    some changes on page layers

--- src/main/java/com/qa/PoshMark/Pages/EntryPage.java
+++ src/main/java/com/qa/PoshMark/Pages/EntryPage.java
@@ -4,7 +4,6 @@ import java.util.Properties;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 import com.qa.PoshMark.Utility.Flat_static;
 import com.qa.PoshMark.Utility.UtilityElementPoshMark;
@@ -13,101 +12,103 @@ import com.qa.PoshMark.Utility.UtilityElementPoshMark;
 
 
 public class EntryPage {
-	
+
 	Properties prop;
 	WebDriver driver;
 	UtilityElementPoshMark elementUtil;
-	
+
 	/**
 	 *  defining my object repo 
 	 */
-	
+
 	By logo = By.xpath("//a//img[@class='header__logo']");
 	By login = By.xpath("//a[text()='Log in']");
-	
-	
+
+
 	/**
 	 * Starting Constructor 
 	 */
-	
-	
+
+
 	public EntryPage (WebDriver driver ) {
 		this.driver = driver;
 		//creating utility object by constructor level and passing driver
 		elementUtil = new UtilityElementPoshMark(driver);
 		prop = new Properties();
 	}
-	
-	
+
+
 	/**
 	 * Now starting Page Actions here 
 	 * @return 
 	 * @return 
 	 */
-	
+
 	public String getPageTitle() {
-		
+
 		String pageTitle = elementUtil.waitForTitlePresent(Flat_static.POSHMARK_MAIN_PAGE_TITLE, 15);
 		System.out.println("main page title is: " +pageTitle);
 		return pageTitle;
 	}
+
+
+	public boolean getEntryPageLogo() {
+
+		//	WebElement frameElement = driver.findElement(By.xpath("//*[@id=\"ssIFrame_google\"]"));
+			
+		
+		driver.switchTo().frame("ssIFrame_google");
+        return elementUtil.isElementDisplayed(logo);
+		
 	
-	
-	public String getEntryPageLogo() {
-		
-	//	WebElement frameElement = driver.findElement(By.xpath("//*[@id=\"ssIFrame_google\"]"));
-	//	driver.switchTo().frame("ssIFrame_google");
-		
-		
-		elementUtil.waitForElementPresent(logo, 15);
-String logotext = elementUtil.doGetText(logo);
-System.out.println("logo text is :" +logotext);
-return logotext;
+//		String logotext = elementUtil.doGetText(logo);
+//		System.out.println("logo text is :" +logotext);
+//		return logotext;
 
 	}
-	
+
 	public LoginPage clickLoginButton() {
 		elementUtil.doClick(login);
-		
+
 		return new LoginPage();
-		
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
